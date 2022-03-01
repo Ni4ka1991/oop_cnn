599aed10f2049f4956dfca6de089cd154da69491
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-03-01 15:28:49 +0200

    LinearNeuron work

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 print( "Hi" )
 
 
-class ConvolutionNeuron:
+class LinearNeuron:
     
     def __init__( self ):
         self.weight = 0
@@ -14,3 +14,20 @@ class ConvolutionNeuron:
 
     def __call__( self, X ):
         return self.forward( X )
+
+
+neuron = LinearNeuron()
+
+neuron.weight = 1
+neuron.bias   = 1
+
+X = [ 1, 44, 45, 46, 47, 2, 3, 4, 5, 6, 0, -1, -2, -3, -4 ]
+Y = []
+
+for x in X:
+    y = neuron( x )
+    Y.append( y )
+
+print( X )
+print( Y )
+

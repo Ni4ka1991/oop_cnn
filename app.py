97b541e7b66a07d5ede3715174e626babf37ee29
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-03-01 15:43:34 +0200

    First step CNN

--- app.py
+++ app.py
@@ -3,11 +3,13 @@
 print( "Hi" )
 
 
-class LinearNeuron:
+class ConvolutionNeuron:
     
     def __init__( self ):
-        self.weight = 0
-        self.bias   = 0
+        self.weight = [[[ 0, 1, 0 ],
+                        [ 0, 1, 0 ],
+                        [ 0, 1, 0 ]
+                    ]]
 
     def forward( self, X ):
         return X * self.weight + self.bias

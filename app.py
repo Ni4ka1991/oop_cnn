24abec103779a7014d22f2951ddb549ced3ca0e6
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-03-02 16:08:04 +0200

    array slicing

--- app.py
+++ app.py
@@ -32,14 +32,14 @@ X = np.array ([
 
 #X_tensor = X.flatten()
 
-neuron = ConvolutionNeuron()
-print( neuron.weights )
-print( type( neuron.weights ))
-print( f"weights dimension >>> {neuron.weights.ndim}")
+#neuron = ConvolutionNeuron()
+#print( neuron.weights )
+#print( type( neuron.weights ))
+#print( f"weights dimension >>> {neuron.weights.ndim}")
 
 print()
 print( f"array X >>>\n{X}" )
-print( f"array X dimension >>> {X.ndim}" )
+#print( f"array X dimension >>> {X.ndim}" )
 #print()
 #print( f"array X_tensor >>>\n{X_tensor}" )
 #print( f"array X_tensor dimension >>> {X_tensor.ndim}" )
@@ -50,21 +50,12 @@ weights = np.array ([
                      [ 0, 1, 0 ]
                    ])
 
-conv1 = np.random.randn( 6, 7 )   #7 colums 6 strings
-print( conv1 )
-print( conv1.ndim )
+#conv1 = np.random.randn( 6, 7 )   #7 colums 6 strings
+#print( conv1 )
+#print( conv1.ndim )
 
-
-
-
-
-
-
-
-#for x in X:
-#    y = neuron( x )
-#    Y.append( y )
-
-#print( X )
-#print( Y )
+x_0 = X[:3]
+print( f"x_0 >>>\n{x_0}" )
+x_0_0 = x_0[ 0:3, 0:3 ]
+print( f"\nx_0_0 >>>\n{x_0_0}" )
 

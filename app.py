df3e8ac1d7fe63ad0c1c055ee885aef407767d1f
Ni4ka1991 <astahova_nica@mail.ru>
2022-03-02 12:12:24 +0200

    Transform srtings to numpay arrays

--- app.py
+++ app.py
@@ -1,35 +1,62 @@
 #!/usr/bin/env python3
-
-print( "Hi" )
-
+import numpy as np
 
 class ConvolutionNeuron:
     
     def __init__( self ):
-        self.weight = [[[ 0, 1, 0 ],
-                        [ 0, 1, 0 ],
-                        [ 0, 1, 0 ]
-                    ]]
+        self.weights = np.array([
+                                 [ 0, 1, 0 ],
+                                 [ 0, 1, 0 ],
+                                 [ 0, 1, 0 ]
+                               ])
 
     def forward( self, X ):
-        return X * self.weight + self.bias
+        return X * self.weight
 
     def __call__( self, X ):
         return self.forward( X )
 
 
-neuron = LinearNeuron()
-
-neuron.weight = 1
-neuron.bias   = 1
-
-X = [ 1, 44, 45, 46, 47, 2, 3, 4, 5, 6, 0, -1, -2, -3, -4 ]
 Y = []
 
-for x in X:
-    y = neuron( x )
-    Y.append( y )
+X = np.array ([
+       [0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 1, 1, 1, 1, 1, 1, 1, 0],
+       [0, 1, 0, 0, 0, 0, 0, 1, 0],
+       [0, 1, 0, 0, 0, 0, 0, 1, 0],
+       [0, 1, 0, 0, 0, 0, 0, 1, 0],
+       [0, 1, 0, 0, 0, 0, 0, 1, 0],
+       [0, 1, 1, 1, 1, 1, 1, 1, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ])
 
+neuron = ConvolutionNeuron()
+print( neuron.weights )
+print( type( neuron.weights ))
+print( f"weights dimension >>> {neuron.weights.ndim}")
+
+print()
 print( X )
-print( Y )
+print( X.ndim )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for x in X:
+#    y = neuron( x )
+#    Y.append( y )
+
+#print( X )
+#print( Y )
 

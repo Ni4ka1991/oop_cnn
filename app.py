68a631ec52883ac4ee45dfdd7630c10d1aaf0f40
Ni4ka1991 <astahova_nica@mail.ru>
2022-03-03 10:19:56 +0200

    Trace array Step by step

--- app.py
+++ app.py
@@ -42,11 +42,25 @@ conv1 = np.random.randn( 6, 7 )
 print( f"\narray X >>>\n{X}" )
 print( f"\nweights >>>\n{weights}")
 
+
+
 x_0 = X[:3]
+print( f"\nx_0 >>>\n{x_0}" )
 x_0_0 = x_0[ 0:3, 0:3 ]
+print( f"\nx_0_0 >>>\n{x_0_0}" )
 y_0 = x_0_0 * weights
+print( f"\ny_0 >>>{y_0}" )
+
+conv1[0][0] = y_0
+
+x_1 = X[1:4]
+print( f"\nx_1 >>>\n{x_1}" )
+x_1_0 = x_1[ 0:3, 0:3 ]
+print( f"\nx_1_0 >>>\n{x_1_0}" )
+y_0 = x_1_0 * weights
 print( y_0 )
 
-conv1[0][0] = y_0.sum()
 print( conv1 )
+
+
 

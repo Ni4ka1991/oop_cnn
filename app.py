840e7f7df7a3a52de348f7c667a12d0e325f2663
Ni4ka1991 <astahova_nica@mail.ru>
2022-03-03 11:52:21 +0200

    First loop for array

--- app.py
+++ app.py
@@ -39,26 +39,20 @@ weights = np.array ([
 #neuron = ConvolutionNeuron()
 conv1 = np.random.randn( 6, 7 )
 
-print( f"\narray X >>>\n{X}" )
-print( f"\nweights >>>\n{weights}")
+#print( f"\narray X >>>\n{X}" )
+#print( f"X shape >>> {X.shape}" ) 
+#print( f"len axis string >>> {len( X )}" )
+#print( f"len axis column >>> {len( X[0] )}" )
+#print( f"\nweights >>>\n{weights}")
 
 
 
-x_0 = X[:3]
-print( f"\nx_0 >>>\n{x_0}" )
-x_0_0 = x_0[ 0:3, 0:3 ]
-print( f"\nx_0_0 >>>\n{x_0_0}" )
-y_0 = x_0_0 * weights
-print( f"\ny_0 >>>{y_0}" )
+for i in range( 0, len( X[0] ) + 1 - 3 ): 
+    x = X[ 0:3, i:( i + 3 ) ]
+    print( f"\nx({i}) >>> \n{x}" )
+    y = ( x * weights ).sum()
+    conv1[0][i] = y
 
-conv1[0][0] = y_0
-
-x_1 = X[1:4]
-print( f"\nx_1 >>>\n{x_1}" )
-x_1_0 = x_1[ 0:3, 0:3 ]
-print( f"\nx_1_0 >>>\n{x_1_0}" )
-y_0 = x_1_0 * weights
-print( y_0 )
 
 print( conv1 )
 
